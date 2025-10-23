85c54bca4b12af2955eda6a793e673471475779c
devcarlos <carlosdrale@gmail.com>
2025-10-23 15:19:27 -0300

    mudando para que acresente minutos

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ function carregar() {
   var msg = document.getElementById("msg");
   var img = document.getElementById("imagem");
   var data = new Date();
+
   var hora = data.getHours();
-  msg.innerHTML = `Agora são ${hora} horas.`;
-    if (hora >= 0 && hora < 12) {
-    // BOM DIA!
+  var minutos = data.getMinutes();
+
+  if (minutos < 10) minutos = "0" + minutos;
+
+  msg.innerHTML = `Agora são ${hora}:${minutos} horas.`;
+
+  if (hora >= 0 && hora < 12) {
     img.src = "img/dia.png";
     document.body.style.background = "#fccd93";
   } else if (hora >= 12 && hora <= 18) {
-    // BOA TARDE!
     img.src = "img/tarde.png";
     document.body.style.background = "#becd8a";
   } else {
-    // BOA NOITE!
     img.src = "img/noite.png";
     document.body.style.background = "#3b4857";
   } 
