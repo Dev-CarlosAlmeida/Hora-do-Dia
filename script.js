96992c81dae455600c5837a29b2a0a62bf95d332
devcarlos <carlosdrale@gmail.com>
2025-10-24 16:41:08 -0300

    deixando Digital

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function carregar() {
   var msg = document.getElementById("msg");
   var img = document.getElementById("imagem");
+
+function pad(num) {
+  if (num < 10) return "0" + num;
+  return num;
+}
   
 function atualizarHora() {
   var data = new Date();
@@ -11,7 +16,25 @@ function atualizarHora() {
   if (minutos < 10) minutos = "0" + minutos;
   if (segundos < 10) segundos = "0" + segundos;
 
-  msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}`;
+  msg.textContent = hora + ":" + minutos + ":" + segundos;
+
+  msg.style.color = "#f00"; 
+  msg.style.background = "#000"; 
+  msg.style.padding = "2px 4px";
+  msg.style.borderRadius = "4px";
+  msg.style.fontFamily = "monospace";
+  msg.style.fontSize = "1vw";
+  msg.style.letterSpacing = "0.3vw";
+  msg.style.textAlign = "center";
+  msg.style.display = "inline-block";
+  msg.style.margin = "20px auto";
+  
+  var section = msg.parentNode;
+  section.style.display = "flex";
+  section.style.flexDirection = "column";
+  section.style.justifyContent = "center";
+  section.style.alignItems = "center";
+
 
   if (hora >= 0 && hora < 12) {
     img.src = "img/dia.png";
